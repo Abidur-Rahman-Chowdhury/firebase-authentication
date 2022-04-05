55084c282f16472d4435943dd7eb5c543f925609
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-04-06 00:07:24 +0600

    added steps of firebase config in _steps.js file

---
+++ src/_steps.js
@@ -0,0 +1,11 @@
+/* 
+* Steps to use firebase
+* 1. create a project on console.firebase.google.com
+* 2. install npm install firebase
+* 4. copy firebase init with config from firebase project settings firebase.init.js
+* 3. Register Web App in firebase
+* 5. export default app
+* 6. import app firebase init.js into your app.js
+* 7. import getAuth from firebase/auth and create auth = getAuth(app)
+* 8. turn on google authentication
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmzvIzJQe9v-N_MQG2a1VQjrklDGHlFLM",
+  authDomain: "react-firebase-authentic-2d8d4.firebaseapp.com",
+  projectId: "react-firebase-authentic-2d8d4",
+  storageBucket: "react-firebase-authentic-2d8d4.appspot.com",
+  messagingSenderId: "381328150151",
+  appId: "1:381328150151:web:12c872896710d664763222"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
