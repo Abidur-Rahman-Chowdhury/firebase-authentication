65e97156d731dcd9cdff38e335c4822a082bcc4c
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-04-06 14:47:49 +0600

    done _step.js how to create firebase app and make google authentication popup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import { getAuth } from "firebase/auth";
 import './App.css';
-import app from './firebase.init';
+
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import app from './firebase.init'
 const auth = getAuth(app);
+
 function App() {
+  const provider = new GoogleAuthProvider();
+
+  const handelGoogleSignIn = () => {
+    signInWithPopup(auth, provider) 
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch(error => {
+      console.error('error' ,error);
+    } )
+  }
+
+  
   return (
     <div className="App">
-      
+     <button onClick={handelGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }
--- src/_steps.js
+++ src/_steps.js
@@ -1,11 +1,15 @@
 /* 
 * Steps to use firebase
 * 1. create a project on console.firebase.google.com
-* 2. install npm install firebase
+* 2. install (npm install firebase)
+* 3. Register web app in firebase
 * 4. copy firebase init with config from firebase project settings firebase.init.js
-* 3. Register Web App in firebase
-* 5. export default app
-* 6. import app firebase init.js into your app.js
-* 7. import getAuth from firebase/auth and create auth = getAuth(app)
-* 8. turn on google authentication
+* 5. Register Web App in firebase
+* 6. export default app from firebase.init.js 
+* 7. import app firebase init.js into your app.js
+* 8. import getAuth from firebase/auth and create const auth = getAuth(app) in app.js file
+* 9. turn on google authentication enable google sign-in
+* 10. create google provider
+* 11. use signInWithPopup and pass (auth, provider)
+* 12. handel .then(if successful) and catch error (if error)
 */
